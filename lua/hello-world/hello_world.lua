4a8c5a242a09cc87e2255533727ecbca9bf9fbb6
Patrick Grämer <mail+github@graemer.org>
2015-08-25 07:34:16 +0200

    started with lua/hello-world

---
+++ lua/hello-world/hello_world.lua
@@ -0,0 +1,11 @@
+HelloWorld = {}
+
+function HelloWorld.hello( name )
+    if not name or name == '' then 
+        name = 'world'
+    end
+    return 'Hello, ' ..  name .. '!'
+end
+
+return HelloWorld
+
