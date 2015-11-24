2fe1bc652cf11d211eb730099a549c6c615fc97d
Patrick Grämer <mail+github@graemer.org>
2015-11-24 07:00:06 +0100

    first iteration of ruby/nth-prime

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,3 +1,22 @@
-class NthPrime
-  
+require 'pp'
+
+class Prime
+  def nth (number)
+    throw 'prime is not possible' unless  number >= 1
+    prime_candidate = 2
+    primes = Array.new
+    while primes.length < number
+      primes.push(prime_candidate) if prime?(prime_candidate)
+      prime_candidate += 1
+    end
+    primes[number -1]
+  end
+
+  def prime?(x)
+    return false if x < 2
+    2.upto(x - 1) do |n|
+      return false if (x % n).zero?
+    end
+    true
+  end
 end
