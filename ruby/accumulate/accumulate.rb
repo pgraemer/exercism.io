eb6b721e3fbfe5cd67ff4255f33df80c807909df
Patrick Grämer <mail+github@graemer.org>
2015-12-07 07:18:10 +0100

    2nd iteration of ruby/accumulate.rb

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,6 +1,8 @@
 require 'pp'
 class Array
   def accumulate(&block)
-    map(&block)
+    self.each_with_index.each_with_object([]) { |(item, index), array|
+      array[index] = block.(item)
+    }
   end
 end
