61d8578ced62e7a162bc31dffeb67b5b3a3ff5da
Patrick Grämer <mail+github@graemer.org>
2016-07-05 06:51:41 +0200

    small changes to leap

--- perl5/leap/Leap.pm
+++ perl5/leap/Leap.pm
@@ -4,10 +4,10 @@ use warnings;
 
 sub is_leap {
     my $year = shift;
-    if ($year % 100 == 0) {
-        return ($year % 400 == 0) ? 1 : 0;
-    }
-    return ($year % 4 == 0) ? 1 : 0;
+    return
+          ($year % 100 == 0 && $year % 400 != 0)  ? 0
+        : ($year %   4 == 0)                      ? 1
+                                                  : 0;
 }
 
 1;
