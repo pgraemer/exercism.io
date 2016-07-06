1b222011ae16c16c2ec353af28325ede1b618269
Patrick Grämer <mail+github@graemer.org>
2016-07-06 06:56:42 +0200

    second iteration of javascript/isogram

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -2,12 +2,7 @@ module.exports = function(word) {
     'use strict';
     return {
         isIsogram : function() {
-            var letters = {}; 
-            word.toLowerCase().replace(/\W/g, '').split('').forEach(function(character) {
-                letters[character] = (parseInt(letters[character]) || 0) + 1;
-            });
-            // Object.values woud be awesome here :(
-            return Object.keys(letters).map(function(k){ return letters[k] }).filter(function(v) { return v > 1;}).length == 0;
+            return word.toLowerCase().replace(/\W/g, '').match(/(\w).*\1+/g) == null;
         }    
     };
 };
